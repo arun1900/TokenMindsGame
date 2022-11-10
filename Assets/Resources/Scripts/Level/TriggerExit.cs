e164a68c82430658b81464fad1307f14488493bf
Arunteja <arunt@pixentech.com>
2022-11-10 11:27:03 +0530

    Added Level chunks Spawning Infinitely

---
+++ Assets/Resources/Scripts/Level/TriggerExit.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Resources.Scripts.Level
+{
+    public class TriggerExit : MonoBehaviour
+    {
+        public float delay = 5f;
+    
+        public delegate void ExitAction();
+        public static event ExitAction OnChunkExited;
+
+        private bool _exited;
+
+        private void OnTriggerExit(Collider other)
+        {
+            var playerTag = other.GetComponent<PlayerTag>();
+            if (playerTag != null)
+            {
+                if (!_exited)
+                {
+                    _exited = true;
+                    OnChunkExited?.Invoke();
+                    StartCoroutine(WaitAndDeactivate());
+                }
+
+
+            }
+        }
+
+        IEnumerator WaitAndDeactivate()
+        {
+            yield return new WaitForSeconds(delay);
+
+            transform.root.gameObject.SetActive(false);
+
+        }
+
+
+
+    }
+}
---
+++ Assets/Resources/Scripts/PlayerTag.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Resources.Scripts
+{
+    public class PlayerTag : MonoBehaviour
+    {
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log("collided with " + collision.gameObject.name);
+        }
+
+    }
+}
